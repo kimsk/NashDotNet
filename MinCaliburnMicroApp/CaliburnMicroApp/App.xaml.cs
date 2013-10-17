88d46a505ddb1182fa770fa8e3d72f0b9147e9de
Karlkim Suwamongkol <karlkim@outlook.com>
2013-10-16 23:29:07 -0500

    Minimum Caliburn.Micro WinRT App
    Windows 8.1 and Caliburn.Micro 1.5.2

---
+++ MinCaliburnMicroApp/CaliburnMicroApp/App.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Caliburn.Micro;
+
+namespace CaliburnMicroApp
+{
+    sealed partial class App
+    {
+        private WinRTContainer _container;
+
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override void Configure()
+        {
+            _container = new WinRTContainer();
+            _container.RegisterWinRTServices();
+
+            _container.PerRequest<MainViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override void PrepareViewFirst(Frame rootFrame)
+        {
+            _container.RegisterNavigationService(rootFrame);
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            //DisplayRootViewFor<MainViewModel>();
+            DisplayRootView<MainView>();
+        }
+    }
+}
---
+++ MinCaliburnMicroApp/CaliburnMicroApp/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CaliburnMicroApp
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            HelloWorld = "Hello Caliburn.Micro";
+        }
+
+        public string HelloWorld { get; set; }
+    }
+}
